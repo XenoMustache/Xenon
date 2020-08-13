e1d47f12510b25405bd2a7591ce3d9616bb6f486
Joshua Clift <xenomustache@gmail.com>
2020-08-13 09:23:30 -0700

    Added basic 2d camera

---
+++ Client/UI/Camera.cs
@@ -0,0 +1,28 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using Xenon.Common;
+
+namespace Xenon.Client.UI {
+	public class Camera : GameObject {
+		public Vector2f target, uiViewSize;
+
+		View camView, uiView;
+
+		public Camera() {
+			camView = new View();
+			uiView = new View();
+		}
+
+		public override void Update() {
+			camView.Center = target;
+		}
+
+		public override void Render() {
+			window.SetView(uiView);
+			uiView.Size = uiViewSize;
+
+			window.SetView(camView);
+			camView.Size = (Vector2f)window.Size;
+		}
+	}
+}
